2f1e7ee3f32b0bdec09357320fb79a58a8a901f6
Amanda Hao <amandahao@gmail.com>
2019-09-18 22:32:43 -0700

    add aggregate for sitesblocked; needs fixing

--- index.js
+++ index.js
@@ -70,11 +70,12 @@ app.get('/analytics/populardomains/:customerid', (req, res, next) => {
 })
 
 app.get('/analytics/sitesblocked/:customerid', (req, res, next) => {
+
 	datamodel
-	.aggregate([
-		{$match: {id:  {$ne: null } } }
-		, {$project: {date: 1, id: 1} }
-		, { $group:
+		.aggregate([
+			{$match: {id:  {$ne: null } } }
+			, {$project: {date: 1, id: 1} }
+			, { $group:
 				{_id : {
 					month: { $month: "$date" }
 					, day: { $dayOfMonth: "$date" }
@@ -82,67 +83,56 @@ app.get('/analytics/sitesblocked/:customerid', (req, res, next) => {
 					}
 					, customers: {$addToSet: "$id" }
 				}
-		}
-	])
-		.find({site: {$ne: null}, id: req.params.customerid})
-		.sort({_id: -1})
-		.select('site time')
-		.limit(100)
+			}
+		])
 		.exec((err, alldata) => {
 			if(err) return res.send("oops");
 			var html = fs.readFileSync("analytics.html", "utf8");
-			var domainData = alldata.reduce((o, r) => {
-			    var domainParts = r.site.split('/');
-			     var domain = domainParts[2];
-			    if(!o[domain]) o[domain] = 0;
-			        o[domain]++;
-			    return o;
-			}, {})
-			html = html.replace(`{{DATA}}`, Object.keys(domainData).map(d => `<div><span>${d}</span><span>${domainData[d]}</span></div>`).join(''));
+			html = html.replace(`{{DATA}}`, JSON.stringify(alldata));
 			res.send(html);
 		})
 })
 
-app.get('/', (req, res, next) => {
-	console.log('home');
-	var geo = geoip.lookup(req.ip);
-	var ua = parser(req.headers['user-agent']);
-	var date = new Date();
-	var site = req.query.site;
-	var action = req.query.action;
-	var id = req.query.id;
-	var time = req.query.time;
-	var sim = req.query.sim;
-	var subject = req.query.subject;
-	var loadsimtime = req.query.loadsimtime;
-	var data = new datamodel({
-		site: site
-		, loadsimtime: loadsimtime || -1
-		, sim: sim || -1
-		, subject: subject
-		, time: time || -1
-		, action: action
-		, id: id
-		, date: date
-		, ip: req.ip
-		, range: geo.range
-		, country: geo.country
-		, region: geo.region
-		, city: geo.city
-		, ll: geo.ll
-		, metro: geo.metro || -1
-		, area: geo.area || -1
-		, eu: geo.eu || -1
-		, timezone: geo.timezone || -1
-		, ua: ua.ua
-		, browser: ua.browser
-		, engine: ua.engine
-		, device: ua.device
-		, cpu: ua.cpu
-	})
-	data.save();
-	res.send("ok");
-});
+// app.get('/', (req, res, next) => {
+// 	console.log('home');
+// 	var geo = geoip.lookup(req.ip) || {};
+// 	var ua = parser(req.headers['user-agent']);
+// 	var date = new Date();
+// 	var site = req.query.site;
+// 	var action = req.query.action;
+// 	var id = req.query.id;
+// 	var time = req.query.time;
+// 	var sim = req.query.sim;
+// 	var subject = req.query.subject;
+// 	var loadsimtime = req.query.loadsimtime;
+// 	var data = new datamodel({
+// 		site: site
+// 		, loadsimtime: loadsimtime || -1
+// 		, sim: sim || -1
+// 		, subject: subject
+// 		, time: time || -1
+// 		, action: action
+// 		, id: id
+// 		, date: date
+// 		, ip: req.ip
+// 		, range: geo.range
+// 		, country: geo.country
+// 		, region: geo.region
+// 		, city: geo.city
+// 		, ll: geo.ll
+// 		, metro: geo.metro || -1
+// 		, area: geo.area || -1
+// 		, eu: geo.eu || -1
+// 		, timezone: geo.timezone || -1
+// 		, ua: ua.ua
+// 		, browser: ua.browser
+// 		, engine: ua.engine
+// 		, device: ua.device
+// 		, cpu: ua.cpu
+// 	})
+// 	data.save();
+// 	res.send("ok");
+// });
 
 function startServer() {
 	server.on('listening', () => {
